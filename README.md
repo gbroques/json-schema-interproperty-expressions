8d2134788420a9fdc61774e87142159e3ca35cf9
G Roques <groques360@gmail.com>
2023-05-25 15:10:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# JSON Schema Interproperty Expressions
+
+**Proof-of-concept** for a custom JSON Schema extension to define interproperty constraints as expressions.
+
+(⚠️ *Edge-cases are not tested.*)
+
+The demo uses an expression in [postfix notation](https://en.wikipedia.org/wiki/Reverse_Polish_notation) as it's easier to evaluate using a [stack](https://en.wikipedia.org/wiki/Stack_(abstract_data_type)), and doesn't require the use of [`eval`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval) which poses security risks.
+
+The motivation behind using [JSON Schema](https://json-schema.org/) is to sync validation rules between an API and user-interface written in different programming languages.
+
+The below GIF demonstrates validating a complex relationship between three related inputs.
+
+The user can adjust any of the three inputs when the constraint is violated.
+
+![Demo](./json-schema-interproperty-expressions-demo.gif)
---
+++ evaluatePostfixExpression.js
@@ -0,0 +1,80 @@
+
+/**
+ * Evaluates an expression in postfix notation.
+ *
+ * The expression MAY contain variables surrounded by curly braces
+ * which are substituted according to the `valueByName` map.
+ *
+ * Tokens (i.e. variables, operators, and operands) SHOULD
+ * be delimited by white-space.
+ *
+ * The time complexity of the algorithm is O(n),
+ * where n is the length of the expression.
+ *
+ * A safe alternative to eval.
+ *
+ * @example
+ * evaluatePostfixExpression('{a} {b} -', {a: "4", b: "3"}) // => 1
+ *
+ * @param {string} expression Expression in postfix notation with variables surrounded
+ *                            by curly braces and tokens delimited by white-space.
+ * @param {Object.<string, string>} valueByName Map of variable values by name.
+ * @returns {*} Result of evaluated expression.
+ *              A number for an arithmetic expression or boolean for relational expression.
+ */
+function evaluatePostfixExpression(expression, valueByName = {}) {
+    const stack = [];
+    const operationByOperator = {
+        "-": (a, b) => a - b,
+        "≥": (a, b) => a >= b
+    };
+    for (const token of tokens(expression, valueByName)) {
+        if (operationByOperator[token]) {
+            const operation = operationByOperator[token];
+            const b = stack.pop();
+            const a = stack.pop();
+            const result = operation(a, b);
+            stack.push(result);
+        } else {
+            const operand = parseFloat(token);
+            if (!Number.isNaN(operand)) {
+                stack.push(operand);
+            }
+        }
+    }
+    return stack.pop();
+}
+
+function* tokens(expression, valueByName) {
+    let token = "";
+    for (const character of substitutedVariables(expression, valueByName)) {
+        if (character !== " ") {
+            token += character;
+        } else {
+            if (token) {
+                yield token;
+                token = "";
+            }
+        }
+    }
+    yield token;
+}
+
+function* substitutedVariables(expression, valueByName) {
+    let start = -1;
+    for (let i = 0; i < expression.length; i++) {
+        const character = expression[i];
+        if (character === "{") {
+            start = i;
+        } else if (character === "}") {
+            const name = expression.substring(start + 1, i);
+            const value = valueByName[name];
+            for (const valueCharacter of value) {
+                yield valueCharacter;
+            }
+            start = -1;
+        } else if (start < 0) {
+            yield character;
+        }
+    }
+}
