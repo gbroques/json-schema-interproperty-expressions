dff51e4e4446528e76441d80dc0834c4fb4733dc
G Roques <groques360@gmail.com>
2023-05-26 13:02:15 -0500

    Make evaluatePostfixExpression configurable

--- evaluatePostfixExpression.js
+++ evaluatePostfixExpression.js
@@ -11,6 +11,8 @@
  *
  * For supported operators, see below `operators` map.
  *
+ * Delimiters and operators may be overridden via `config`.
+ *
  * Errors are returned as part of the result instead of thrown.
  *
  * The time complexity of the algorithm is O(n),
@@ -37,10 +39,24 @@
  *                            by curly braces and tokens delimited by white-space.
  * @param {Object.<string, *>} variables Map of variable values by name.
  *                                       Nested values and dot notation are not supported.
+ * @param {Object} config
+ * @param {Object.<string, function>} config.operators Map of additional operators, or override existing operators.
+ * @param {string} config.variableStartDelimiter Variable start delimiter. Defaults to "{".
+ * @param {string} config.variableEndDelimiter Variable end delimiter. Defaults to "}".
+ * @param {string} config.tokenDelimiter String to delimit tokens. Defaults to " ".
  * @returns {Array} Two element array containing result of evaluated expression, and potential parsing error.
  *                  Result can be a number for an arithmetic expression or boolean for relational expression.
  */
-export default function evaluatePostfixExpression(expression, variables = {}) {
+export default function evaluatePostfixExpression(
+    expression,
+    variables = {},
+    config = {
+        operators: {},
+        variableStartDelimiter: "{",
+        variableEndDelimiter: "}",
+        tokenDelimiter: " "
+    }
+) {
     const stack = [];
     const operators = {
         // Arithmetic operators
@@ -57,9 +73,11 @@ export default function evaluatePostfixExpression(expression, variables = {}) {
         ">": (a, b) => a > b,
         "≥": (a, b) => a >= b,
         "=": (a, b) => a == b,
-        "≠": (a, b) => a != b
+        "≠": (a, b) => a != b,
+
+        ...config.operators
     };
-    for (const token of tokens(expression, variables)) {
+    for (const token of tokens(expression, variables, config)) {
         const operator = operators[token];
         if (operator) {
             const b = stack.pop();
@@ -77,17 +95,21 @@ export default function evaluatePostfixExpression(expression, variables = {}) {
     return [result, error];
 }
 
-function* tokens(expression, variables) {
+function* tokens(expression, variables, config) {
+    const {
+        variableStartDelimiter, variableEndDelimiter, tokenDelimiter
+    } = config;
+
     let token = "";
     for (const character of expression) {
-        if (character === "}") {
+        if (character === variableEndDelimiter) {
             token = variables[token];
-        } else if (character === " ") {
+        } else if (character === tokenDelimiter) {
             if (token !== "") {
                 yield token;
                 token = "";
             }
-        } else if (character !== "{") {
+        } else if (character !== variableStartDelimiter) {
             token += character;
         }
     }
